fa9c8107323878d5c18345d4f8a5acddd0732021
Carmen <ac21002ts@esfe.agape.edu.sv>
2022-08-17 17:59:16 -0500

    crear 2 clases

--- ProyectodealimentacionS-AccesoDatos/src/proyectodealimentacions/accesodatos/ControlPublicacionDAL.java
+++ ProyectodealimentacionS-AccesoDatos/src/proyectodealimentacions/accesodatos/ControlPublicacionDAL.java
@@ -1,14 +1,222 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package proyectodealimentacions.accesodatos;
-
-/**
- *
- * @author Alumno
- */
+import java.util.*; // Utilizar la utileria de java 
+import java.sql.*;
+import proyectodealimentacions.entidadesdenegocio.*;
 public class ControlPublicacionDAL {
     
+    public static String encriptarMD5(String txt) throws Exception {
+        try {
+            StringBuffer sb;
+            java.security.MessageDigest md = java.security.MessageDigest
+                    .getInstance("MD5");
+            byte[] array = md.digest(txt.getBytes());
+            sb = new StringBuffer();
+            for (int i = 0; i < array.length; ++i) {
+                sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100)
+                        .substring(1, 3));
+            }
+            return sb.toString();
+        } catch (java.security.NoSuchAlgorithmException ex) {
+            throw ex;
+        }
+    }
+    static String obtenerCampos() {
+        return "c.Id,c.Nombre,c.Descripcion";
+    }
+
+    // Metodo para obtener el SELECT a la tabla Rol y el TOP en el caso que se utilice una base de datos SQL SERVER
+    private static String obtenerSelect(ControlPublicacion cControl) {
+        String sql;
+        sql = "SELECT ";
+        if (cControl.getTop_aux() > 0 && comunDB.TIPODB == comunDB.TipoDB.SQLSERVER) {
+            // Agregar el TOP a la consulta SELECT si el gestor de base de datos es SQL SERVER y "getTop_aux" es mayor a cero
+            sql += "TOP " + cControl.getTop_aux() + " ";
+        }
+        sql += (obtenerCampos() + " FROM Control c"); // Agregar los campos de la tabla de Rol mas el FROM a la tabla Rol con su alias "r"
+        return sql;
+    }
+    private static String agregarOrderBy(ControlPublicacion cControl) {
+        String sql = " ORDER BY u.Id DESC";
+        if (cControl.getTop_aux() > 0 && comunDB.TIPODB == comunDB.TipoDB.MYSQL) {
+            // Agregar el LIMIT a la consulta SELECT de la tabla de Usuario en el caso que getTop_aux() sea mayor a cero y el gestor de base de datos
+            // sea MYSQL
+            sql += " LIMIT " + cControl.getTop_aux() + " ";
+        }
+        return sql;
+    }
+   public static int crear(ControlPublicacion cControl) throws Exception {
+        int result;
+        String sql;
+        try (Connection conn = comunDB.obtenerConexion();) { // Obtener la conexion desde la clase ComunDB y encerrarla en try para cierre automatico
+            sql = "INSERT INTO Control(Nombre) VALUES(?), Control(Descripcion) VALUES(?)"; // Definir la consulta INSERT a la tabla de Rol utilizando el simbolo ? para enviar parametros
+            try (PreparedStatement ps = comunDB.createPreparedStatement(conn, sql);) { // Obtener el PreparedStatement desde la clase ComunDB
+                ps.setString(1, cControl.getNombre()); // Agregar el parametro a la consulta donde estan el simbolo ? #1  
+                ps.setString(2, cControl.getDescripcion()); // Agregar el parametro a la consulta donde estan el simbolo ? #1  
+                result = ps.executeUpdate();// Ejecutar la consulta INSERT en la base de datos
+                ps.close(); // Cerrar el PreparedStatement
+            } catch (SQLException ex) {
+                throw ex; // Enviar al siguiente metodo el error al ejecutar PreparedStatement en el caso que suceda 
+            }
+            conn.close(); // Cerrar la conexion a la base de datos
+        } catch (SQLException ex) {
+            throw ex; // Enviar al siguiente metodo el error al obtener la conexion en el caso que suceda 
+        }
+        return result; // Retornar el numero de fila afectadas en el INSERT en la base de datos 
+    }
+   public static int modificar(ControlPublicacion cControl) throws Exception {
+        int result;
+        String sql;
+        try (Connection conn = comunDB.obtenerConexion();) { // Obtener la conexion desde la clase ComunDB y encerrarla en try para cierre automatico
+            sql = "UPDATE   Control SET Nombre=? SET dESCRIPCION =? WHERE Id=?"; // Definir la consulta UPDATE a la tabla de Rol utilizando el simbolo ? para enviar parametros
+            try (PreparedStatement ps = comunDB.createPreparedStatement(conn, sql);) { // Obtener el PreparedStatement desde la clase ComunDB
+                ps.setString(1, cControl.getNombre()); 
+                ps.setString(2, cControl.getDescripcion());// Agregar el parametro a la consulta donde estan el simbolo ? #1  
+                ps.setInt(3, cControl.getId()); // Agregar el parametro a la consulta donde estan el simbolo ? #2  
+                result = ps.executeUpdate(); // Ejecutar la consulta UPDATE en la base de datos
+                ps.close(); // Cerrar el PreparedStatement
+            } catch (SQLException ex) {
+                throw ex;  // Enviar al siguiente metodo el error al ejecutar PreparedStatement en el caso que suceda 
+            }
+            conn.close(); // Cerrar la conexion a la base de datos
+        } catch (SQLException ex) {
+            throw ex; // Enviar al siguiente metodo el error al obtener la conexion en el caso que suceda 
+        }
+        return result; // Retornar el numero de fila afectadas en el UPDATE en la base de datos 
+    }
+   public static int eliminar(ControlPublicacion cControl) throws Exception {
+        int result;
+        String sql;
+        try (Connection conn = comunDB.obtenerConexion();) { // Obtener la conexion desde la clase ComunDB y encerrarla en try para cierre automatico
+            sql = "DELETE FROM Control WHERE Id=?";  // Definir la consulta DELETE a la tabla de Rol utilizando el simbolo ? para enviar parametros
+            try (PreparedStatement ps = comunDB.createPreparedStatement(conn, sql);) { // Obtener el PreparedStatement desde la clase ComunDB
+                ps.setInt(1, cControl.getId()); // Agregar el parametro a la consulta donde estan el simbolo ? #1 
+                result = ps.executeUpdate();  // Ejecutar la consulta DELETE en la base de datos
+                ps.close(); // Cerrar el PreparedStatement
+            } catch (SQLException ex) {
+                throw ex; // Enviar al siguiente metodo el error al ejecutar PreparedStatement en el caso que suceda
+            }
+            conn.close();  // Cerrar la conexion a la base de datos
+        } catch (SQLException ex) {
+            throw ex; // Enviar al siguiente metodo el error al obtener la conexion  de la clase ComunDB en el caso que suceda 
+        }
+        return result; // Retornar el numero de fila afectadas en el DELETE en la base de datos 
+    }
+   static int asignarDatosResultSet(ControlPublicacion cControl, ResultSet pResultSet, int pIndex) throws Exception {
+        //  SELECT r.Id(indice 1),r.Nombre(indice 2) * FROM Rol
+        pIndex++;
+        cControl.setId(pResultSet.getInt(pIndex)); // index 1
+        pIndex++;
+        cControl.setNombre(pResultSet.getString(pIndex)); 
+        pIndex++;
+        cControl.setDescripcion(pResultSet.getString(pIndex));// index 2
+        return pIndex;
+    }
+   private static void obtenerDatos(PreparedStatement pPS, ArrayList<ControlPublicacion> cControles) throws Exception {
+        try (ResultSet resultSet = comunDB.obtenerResultSet(pPS);) { // obtener el ResultSet desde la clase ComunDB
+            while (resultSet.next()) { // Recorrer cada una de la fila que regresa la consulta  SELECT de la tabla Rol
+                ControlPublicacion control = new ControlPublicacion(); 
+                asignarDatosResultSet(control, resultSet, 0); // Llenar las propiedaddes de la Entidad Rol con los datos obtenidos de la fila en el ResultSet
+                cControles.add(control); // Agregar la entidad Rol al ArrayList de Rol
+            }
+            resultSet.close(); // Cerrar el ResultSet
+        } catch (SQLException ex) {
+            throw ex; // Enviar al siguiente metodo el error al obtener ResultSet de la clase ComunDB   en el caso que suceda 
+        }
+    }
+   public static ControlPublicacion obtenerPorId(ControlPublicacion cControl) throws Exception {
+        ControlPublicacion control = new ControlPublicacion();
+        ArrayList<ControlPublicacion> Controles = new ArrayList();
+        try (Connection conn = comunDB.obtenerConexion();) { // Obtener la conexion desde la clase ComunDB y encerrarla en try para cierre automatico
+            String sql = obtenerSelect(cControl); // Obtener la consulta SELECT de la tabla Rol
+            sql += " WHERE r.Id=?"; // Concatenar a la consulta SELECT de la tabla Rol el WHERE 
+            try (PreparedStatement ps = comunDB.createPreparedStatement(conn, sql);) { // Obtener el PreparedStatement desde la clase ComunDB
+                ps.setInt(1, cControl.getId()); // Agregar el parametro a la consulta donde estan el simbolo ? #1 
+                obtenerDatos(ps, Controles); // Llenar el ArrayList de Rol con las fila que devolvera la consulta SELECT a la tabla de Rol
+                ps.close(); // Cerrar el PreparedStatement
+            } catch (SQLException ex) {
+                throw ex;  // Enviar al siguiente metodo el error al ejecutar PreparedStatement en el caso que suceda
+            }
+            conn.close();  // Cerrar la conexion a la base de datos
+        }
+        catch (SQLException ex) {
+            throw ex; // Enviar al siguiente metodo el error al obtener la conexion  de la clase ComunDB en el caso que suceda
+        }
+        if (Controles.size() > 0) { // Verificar si el ArrayList de Rol trae mas de un registro en tal caso solo debe de traer uno
+            control = Controles.get(0); // Si el ArrayList de Rol trae un registro o mas obtenemos solo el primero 
+        }
+        return control; // Devolver el rol encontrado por Id 
+    }
+   
+   public static ArrayList<ControlPublicacion> obtenerTodos() throws Exception {
+        ArrayList<ControlPublicacion> controles;
+        controles = new ArrayList<>();
+        try (Connection conn = comunDB.obtenerConexion();) {// Obtener la conexion desde la clase ComunDB y encerrarla en try para cierre automatico
+            String sql = obtenerSelect(new ControlPublicacion());  // Obtener la consulta SELECT de la tabla Rol
+            sql += agregarOrderBy(new ControlPublicacion());  // Concatenar a la consulta SELECT de la tabla Rol el ORDER BY por Id 
+            try (PreparedStatement ps = comunDB.createPreparedStatement(conn, sql);) { // Obtener el PreparedStatement desde la clase ComunDB
+                obtenerDatos(ps, controles); // Llenar el ArrayList de Rol con las fila que devolvera la consulta SELECT a la tabla de Rol
+                ps.close(); // Cerrar el PreparedStatement
+            } catch (SQLException ex) {
+                throw ex; // Enviar al siguiente metodo el error al ejecutar PreparedStatement en el caso que suceda
+            }
+            conn.close(); // Cerrar la conexion a la base de datos
+        } 
+        catch (SQLException ex) {
+            throw ex; // Enviar al siguiente metodo el error al obtener la conexion  de la clase ComunDB en el caso que suceda
+        }
+        return controles; // Devolver el ArrayList de Rol
+    }
+   static void querySelect(ControlPublicacion cControl, comunDB.UtilQuery pUtilQuery) throws SQLException {
+        PreparedStatement statement = pUtilQuery.getStatement(); // Obtener el PreparedStatement al cual aplicar los parametros
+        if (cControl.getId() > 0) { // Verificar si se va incluir el campo Id en el filtro de la consulta SELECT de la tabla de Rol
+            pUtilQuery.AgregarWhereAnd(" c.Id=? "); // Agregar el campo Id al filtro de la consulta SELECT y agregar en el WHERE o AND
+            if (statement != null) { 
+                // Agregar el parametro del campo Id a la consulta SELECT de la tabla de Rol
+                statement.setInt(pUtilQuery.getNumWhere(), cControl.getId()); 
+            }
+        }
+        // Verificar si se va incluir el campo Nombre en el filtro de la consulta SELECT de la tabla de Rol
+        if (cControl.getNombre() != null && cControl.getNombre().trim().isEmpty() == false) {
+            pUtilQuery.AgregarWhereAnd(" c.Nombre LIKE ? "); // Agregar el campo Nombre al filtro de la consulta SELECT y agregar en el WHERE o AND
+            if (statement != null) {
+                // Agregar el parametro del campo Nombre a la consulta SELECT de la tabla de Rol
+                statement.setString(pUtilQuery.getNumWhere(), "%" + cControl.getNombre() + "%"); 
+            }
+        }
+         if (cControl.getDescripcion() != null && cControl.getDescripcion().trim().isEmpty() == false) {
+            pUtilQuery.AgregarWhereAnd(" c.Descripcion LIKE ? "); // Agregar el campo Nombre al filtro de la consulta SELECT y agregar en el WHERE o AND
+            if (statement != null) {
+                // Agregar el parametro del campo Nombre a la consulta SELECT de la tabla de Rol
+                statement.setString(pUtilQuery.getNumWhere(), "%" + cControl.getDescripcion() + "%"); 
+            }
+        }
+    }
+   
+   public static ArrayList<ControlPublicacion> buscar(ControlPublicacion cControl) throws Exception {
+        ArrayList<ControlPublicacion> controles = new ArrayList();
+        try (Connection conn = comunDB.obtenerConexion();) { // Obtener la conexion desde la clase ComunDB y encerrarla en try para cierre automatico
+            String sql = obtenerSelect(cControl); // Obtener la consulta SELECT de la tabla Rol
+            comunDB comundb = new comunDB();
+            comunDB.UtilQuery utilQuery = comundb.new UtilQuery(sql, null, 0); 
+            querySelect(cControl, utilQuery); // Asignar el filtro a la consulta SELECT de la tabla de Rol 
+            sql = utilQuery.getSQL(); 
+            sql += agregarOrderBy(cControl); // Concatenar a la consulta SELECT de la tabla Rol el ORDER BY por Id
+            try (PreparedStatement ps = comunDB.createPreparedStatement(conn, sql);) { // Obtener el PreparedStatement desde la clase ComunDB
+                utilQuery.setStatement(ps);
+                utilQuery.setSQL(null);
+                utilQuery.setNumWhere(0); 
+                querySelect(cControl, utilQuery);  // Asignar los parametros al PreparedStatement de la consulta SELECT de la tabla de Rol
+                obtenerDatos(ps, controles); // Llenar el ArrayList de Rol con las fila que devolvera la consulta SELECT a la tabla de Rol
+                ps.close(); // Cerrar el PreparedStatement
+            } catch (SQLException ex) {
+                throw ex;  // Enviar al siguiente metodo el error al ejecutar PreparedStatement en el caso que suceda
+            }
+            conn.close(); // Cerrar la conexion a la base de datos
+        }
+        catch (SQLException ex) {
+            throw ex; // Enviar al siguiente metodo el error al obtener la conexion  de la clase ComunDB en el caso que suceda
+        }
+        return controles; // Devolver el ArrayList de Rol
+    }
 }
+
