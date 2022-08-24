b0f3c96ea423e6c835aa5a793436cf896fd5d052
Carmen <ac21002ts@esfe.agape.edu.sv>
2022-08-24 17:13:54 -0500

    creacion de las clases appweb

--- ProyectodealimentacionS-AppWeb/src/java/proyectoalimentacion/appweb/controllers/RecetasServlet.java
+++ ProyectodealimentacionS-AppWeb/src/java/proyectoalimentacion/appweb/controllers/RecetasServlet.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package proyectoalimentacion.appweb.controllers;
 
 import java.io.IOException;
@@ -12,77 +7,202 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import proyectoalimentacion.appweb.utils.SessionUser;
+import proyectoalimentacion.appweb.utils.Utilidad;
+import proyectodealimentacions.accesodatos.RecetasDAL;
+import proyectodealimentacions.accesodatos.RolDAL;
+import proyectodealimentacions.entidadesdenegocio.Recetas;
+import proyectodealimentacions.entidadesdenegocio.Rol;
 
-/**
- *
- * @author Alumno
- */
 @WebServlet(name = "RecetasServlet", urlPatterns = {"/RecetasServlet"})
 public class RecetasServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet RecetasServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet RecetasServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+    private Recetas obtenerRecetas(HttpServletRequest request) {
+        String accion = Utilidad.getParameter(request, "accion", "index");
+        Recetas receta = new Recetas();
+        if (accion.equals("create") == false) {
+            receta.setId(Integer.parseInt(Utilidad.getParameter(request, "id", "0")));
+        }
+
+        receta.setNombre(Utilidad.getParameter(request, "nombre", ""));
+        if (accion.equals("index")) {
+            receta.setTop_aux(Integer.parseInt(Utilidad.getParameter(request, "top_aux", "10")));
+            receta.setTop_aux(receta.getTop_aux() == 0 ? Integer.MAX_VALUE : receta.getTop_aux());
+        }
+        receta.setIngredientes(Utilidad.getParameter(request, "ingrediente", ""));
+        if (accion.equals("index")) {
+            receta.setTop_aux(Integer.parseInt(Utilidad.getParameter(request, "top_aux", "10")));
+            receta.setTop_aux(receta.getTop_aux() == 0 ? Integer.MAX_VALUE : receta.getTop_aux());
+        }
+        receta.setPreparacion(Utilidad.getParameter(request, "preparacion", ""));
+        if (accion.equals("index")) {
+            receta.setTop_aux(Integer.parseInt(Utilidad.getParameter(request, "top_aux", "10")));
+            receta.setTop_aux(receta.getTop_aux() == 0 ? Integer.MAX_VALUE : receta.getTop_aux());
+        }
+        receta.setDificultad(Utilidad.getParameter(request, "dificultad", ""));
+        if (accion.equals("index")) {
+            receta.setTop_aux(Integer.parseInt(Utilidad.getParameter(request, "top_aux", "10")));
+            receta.setTop_aux(receta.getTop_aux() == 0 ? Integer.MAX_VALUE : receta.getTop_aux());
+        }
+        return receta;
+    }
+    
+    private void doGetRequestIndex(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            Recetas receta = new Recetas();
+            receta.setTop_aux(10);
+            ArrayList<Recetas> recetas = RecetasDAL.buscar(receta);
+            request.setAttribute("recetas", recetas);
+            request.setAttribute("top_aux", receta.getTop_aux());             
+            request.getRequestDispatcher("Views/Recetas/index.jsp").forward(request, response);
+        } catch (Exception ex) {
+            Utilidad.enviarError(ex.getMessage(), request, response);
         }
     }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
+    private void doPostRequestIndex(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            Recetas receta = obtenerRecetas(request);
+            ArrayList<Recetas> recetas = RecetasDAL.buscar(receta);
+            request.setAttribute("recetas", recetas);
+            request.setAttribute("top_aux", receta.getTop_aux());
+            request.getRequestDispatcher("Views/Recetas/index.jsp").forward(request, response);
+        } catch (Exception ex) { 
+            Utilidad.enviarError(ex.getMessage(), request, response);
+        }
+    }
+      private void doGetRequestCreate(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("Views/Recetas/create.jsp").forward(request, response);
+    }
+    
+    private void doPostRequestCreate(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            Recetas receta = obtenerRecetas(request);
+            int result = RecetasDAL.crear(receta);
+            if (result != 0) {
+                request.setAttribute("accion", "index");
+                doGetRequestIndex(request, response);
+            } else {
+                Utilidad.enviarError("No se logro registrar un nuevo registro", request, response);
+            }
+        } catch (Exception ex) {
+            Utilidad.enviarError(ex.getMessage(), request, response);
+        }
+    }
+    private void requestObtenerPorId(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            Recetas receta = obtenerRecetas(request);
+            Recetas recetas_result = RecetasDAL.obtenerPorId(receta);
+            if (recetas_result.getId() > 0) {
+                request.setAttribute("rol", recetas_result);
+            } else {
+                Utilidad.enviarError("El Id:" + receta.getId() + " no existe en la tabla de Receta", request, response);
+            }
+        } catch (Exception ex) {
+            Utilidad.enviarError(ex.getMessage(), request, response);
+        }
+    }
+     private void doGetRequestEdit(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        requestObtenerPorId(request, response);
+        request.getRequestDispatcher("Views/Receta/edit.jsp").forward(request, response);
+    }
+     private void doPostRequestEdit(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            Recetas receta = obtenerRecetas(request);
+            int result = RecetasDAL.modificar(receta);
+            if (result != 0) {
+                request.setAttribute("accion", "index");
+                doGetRequestIndex(request, response);
+            } else {
+                Utilidad.enviarError("No se logro actualizar el registro", request, response);
+            }
+        } catch (Exception ex) {
+            // Enviar al jsp de error si hay un Exception
+            Utilidad.enviarError(ex.getMessage(), request, response);
+        }
+    }
+    private void doGetRequestDetails(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        requestObtenerPorId(request, response);
+        request.getRequestDispatcher("Views/Recetas/details.jsp").forward(request, response);
+    }
+    
+    private void doGetRequestDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        requestObtenerPorId(request, response);
+        request.getRequestDispatcher("Views/Recetas/delete.jsp").forward(request, response);
+    }
+    private void doPostRequestDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            Recetas receta = obtenerRecetas(request);
+            int result = RecetasDAL.eliminar(receta);
+            if (result != 0) {
+                request.setAttribute("accion", "index");
+                doGetRequestIndex(request, response);
+            } else {
+                Utilidad.enviarError("No se logro eliminar el registro", request, response);
+            }
+        } catch (Exception ex) {
+            Utilidad.enviarError(ex.getMessage(), request, response);
+        }
+    }
+     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        SessionUser.authorize(request, response, () -> {
+            String accion = Utilidad.getParameter(request, "accion", "index");
+            switch (accion) {
+                case "index":
+                    request.setAttribute("accion", accion);
+                    doGetRequestIndex(request, response);
+                    break;
+                case "create":
+                    request.setAttribute("accion", accion);
+                    doGetRequestCreate(request, response);
+                    break;
+                case "edit":
+                    request.setAttribute("accion", accion);
+                    doGetRequestEdit(request, response);
+                    break;
+                case "delete":
+                    request.setAttribute("accion", accion);
+                    doGetRequestDelete(request, response);
+                    break;
+                case "details":
+                    request.setAttribute("accion", accion);
+                    doGetRequestDetails(request, response);
+                    break;
+                default:
+                    request.setAttribute("accion", accion);
+                    doGetRequestIndex(request, response);
+            }
+        });
     }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
+        @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        SessionUser.authorize(request, response, () -> {
+            String accion = Utilidad.getParameter(request, "accion", "index");
+            switch (accion) {
+                case "index":
+                    request.setAttribute("accion", accion);
+                    doPostRequestIndex(request, response);
+                    break;
+                case "create":
+                    request.setAttribute("accion", accion);
+                    doPostRequestCreate(request, response);
+                    break;
+                case "edit":
+                    request.setAttribute("accion", accion);
+                    doPostRequestEdit(request, response);
+                    break;
+                case "delete":
+                    request.setAttribute("accion", accion);
+                    doPostRequestDelete(request, response);
+                    break;
+                default:
+                    request.setAttribute("accion", accion);
+                    doGetRequestIndex(request, response);
+            }
+        });
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
